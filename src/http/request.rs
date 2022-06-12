747b85d823bd7f21b894fb95b4377b2f42249897
amangilev <artmnglv@gmail.com>
2022-06-12 17:31:44 +0500

    Make separate lib for requests

---
+++ src/http/request.rs
@@ -0,0 +1,32 @@
+
+use std::{net::TcpStream, io::{Write, Read}};
+
+use dns_lookup::lookup_host;
+
+use crate::url::parser::{self};
+
+pub fn get(url: &str) -> String {
+    let url = parser::parse(url).unwrap();
+
+    let ip = lookup_host(&url.host).unwrap().first().unwrap().to_string();
+    let ip_with_port = [&ip, ":", "80"].concat();
+
+    let mut stream = match TcpStream::connect(ip_with_port) {
+        Ok(stream) => stream,
+        Err(_) => panic!("Problem with connection")
+    };
+
+    let method = format!("GET {} HTTP/1.0\r\n", url.path);
+    let host = format!("Host: {}\r\n\r\n", url.host);
+    
+    let mut request = String::new();
+    request += &method;
+    request += &host;
+
+    stream.write(request.as_bytes()).unwrap();
+
+    let mut str_buffer = String::new();
+    stream.read_to_string(&mut str_buffer).unwrap();
+
+    return str_buffer;
+}
--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,10 @@
 mod url;
+pub mod http;
 
-use url::parser;
-
-use dns_lookup::lookup_host;
-use std::{net::{TcpStream}, io::{Write, Read}, fmt::format};
+use http::request;
 
 fn main() {
-    let url = parser::parse("http://example.org/").unwrap();
+    let response = request::get("http://example.org/");
 
-    let ip = lookup_host(&url.host).unwrap().first().unwrap().to_string();
-    let ip_with_port = [&ip, ":", "80"].concat();
-
-    if let Ok(mut stream) = TcpStream::connect(ip_with_port) {
-        println!("Connected to the server!");
-        
-        let method = format!("GET {} HTTP/1.0\r\n", url.path);
-        let host = format!("Host: {}\r\n\r\n", url.host);
-        let mut request = String::new();
-        request += &method;
-        request += &host;
-
-        println!("{}", request);
-
-        let sent = stream.write(request.as_bytes());
-
-        println!("{}", sent.unwrap());
-
-        let mut str_buffer = String::new();
-
-        stream.read_to_string(&mut str_buffer);
-
-        println!("{}", str_buffer);
-    } else {
-        println!("Couldn't connect to server...");
-    }
+    println!("{}", response)
 }
