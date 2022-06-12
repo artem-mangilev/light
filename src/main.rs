903731e63ce0bcf392929b0355dda89222c2070d
amangilev <artmnglv@gmail.com>
2022-06-12 16:34:30 +0500

    Make first request

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,35 @@ mod url;
 use url::parser;
 
 use dns_lookup::lookup_host;
+use std::{net::{TcpStream}, io::{Write, Read}, fmt::format};
 
 fn main() {
     let url = parser::parse("http://example.org/").unwrap();
 
-    let host = lookup_host(&url.host).unwrap();
+    let ip = lookup_host(&url.host).unwrap().first().unwrap().to_string();
+    let ip_with_port = [&ip, ":", "80"].concat();
 
-    for addr in host.iter() {
-        println!("ip address: {}", addr);
+    if let Ok(mut stream) = TcpStream::connect(ip_with_port) {
+        println!("Connected to the server!");
+        
+        let method = format!("GET {} HTTP/1.0\r\n", url.path);
+        let host = format!("Host: {}\r\n\r\n", url.host);
+        let mut request = String::new();
+        request += &method;
+        request += &host;
+
+        println!("{}", request);
+
+        let sent = stream.write(request.as_bytes());
+
+        println!("{}", sent.unwrap());
+
+        let mut str_buffer = String::new();
+
+        stream.read_to_string(&mut str_buffer);
+
+        println!("{}", str_buffer);
+    } else {
+        println!("Couldn't connect to server...");
     }
 }
